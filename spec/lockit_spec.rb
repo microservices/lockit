99cd533d8403e53d468f0ad4e74d603cf941025f
Bess Sadler <bess@stanford.edu>
2011-06-09 14:50:13 -0500

    I rewrote the tests to properly delete the temp directories they create

--- spec/lockit_spec.rb
+++ spec/lockit_spec.rb
@@ -7,18 +7,20 @@ describe LockIt::Dir do
   describe "locking and unlocking" do
     
     it "is unlocked by default" do
-      dir = Dir.mktmpdir
-      lockit_dir = LockIt::Dir.new(dir)
-      lockit_dir.locked?.should eql(false)
+      Dir.mktmpdir do |d|
+        lockit_dir = LockIt::Dir.new(d)
+        lockit_dir.locked?.should eql(false)
+      end
     end
 
     it "can be locked and unlocked" do
-      dir = Dir.mktmpdir
-      lockit_dir = LockIt::Dir.new(dir)
-      lockit_dir.lock
-      lockit_dir.locked?.should eql(true)
-      lockit_dir.unlock
-      lockit_dir.locked?.should eql(false)
+      Dir.mktmpdir do |d|      
+        lockit_dir = LockIt::Dir.new(d)
+        lockit_dir.lock
+        lockit_dir.locked?.should eql(true)
+        lockit_dir.unlock
+        lockit_dir.locked?.should eql(false)
+      end
     end
     
   end
@@ -26,35 +28,42 @@ describe LockIt::Dir do
   describe "#try_lock" do
         
     it "tries the lock & returns false if it is already locked" do
-      lockit_dir = LockIt::Dir.new(Dir.mktmpdir)
-      lockit_dir.lock
-      lockit_dir.try_lock.should eql(false)
+      Dir.mktmpdir do |d|      
+        lockit_dir = LockIt::Dir.new(d)
+        lockit_dir.lock
+        lockit_dir.try_lock.should eql(false)
+      end
     end
     
     it "tries the lock & unlocks the dir if it isn't yet locked" do
-      lockit_dir = LockIt::Dir.new(Dir.mktmpdir)
-      lockit_dir.try_lock
-      lockit_dir.locked?.should eql(true)
+      Dir.mktmpdir do |d|      
+        lockit_dir = LockIt::Dir.new(d)
+        lockit_dir.try_lock
+        lockit_dir.locked?.should eql(true)
+      end
     end
     
   end
   
   describe "#lock_info" do
     
-    before(:all) do
-      @dir = Dir.mktmpdir
-      @lockit_dir = LockIt::Dir.new(@dir)
-      @lockit_dir.try_lock
-      @info = @lockit_dir.lock_info
-    end
-    
     it "knows the location of the lockfile" do
-      @info[:file].should eql("#{@dir}/#{LockIt::FILENAME}")
+      Dir.mktmpdir do |d|      
+        lockit_dir = LockIt::Dir.new(d)
+        lockit_dir.try_lock
+        info = lockit_dir.lock_info
+        info[:file].should eql("#{d}/#{LockIt::FILENAME}")
+      end
     end
     
     it "knows when it was created" do
-      obtained = DateTime.parse(@info[:obtained])
-      obtained.should be_instance_of(DateTime)
+      Dir.mktmpdir do |d|      
+        lockit_dir = LockIt::Dir.new(d)
+        lockit_dir.try_lock
+        info = lockit_dir.lock_info
+        obtained = DateTime.parse(info[:obtained])
+        obtained.should be_instance_of(DateTime)
+      end
     end
     
   end
@@ -62,20 +71,32 @@ describe LockIt::Dir do
   describe "#revise_lock" do
     
     it "returns false if called on an unlocked directory" do
-      lockit_dir = LockIt::Dir.new(Dir.mktmpdir)
-      lockit_dir.locked?.should eql(false)
-      release_time = Time.now + 60
-      lockit_dir.revise_lock(:release => release_time).should eql(false)
+      Dir.mktmpdir do |d|      
+        lockit_dir = LockIt::Dir.new(d)
+        lockit_dir.locked?.should eql(false)
+        release_time = Time.now + 60
+        lockit_dir.revise_lock(:release => release_time).should eql(false)
+      end
     end
     
     it "adds a release time to a locked directory" do
-      lockit_dir = LockIt::Dir.new(Dir.mktmpdir)
-      lockit_dir.lock
-      lockit_dir.locked?.should eql(true)
-      release_time = Time.now + 60
-      lockit_dir.revise_lock(:release => release_time)
-      @info = lockit_dir.lock_info
-      DateTime.parse(@info[:release]).should be_instance_of(DateTime)
+      Dir.mktmpdir do |d|      
+        lockit_dir = LockIt::Dir.new(d)
+        lockit_dir.lock
+        lockit_dir.locked?.should eql(true)
+        release_time = Time.now + 60
+        lockit_dir.revise_lock(:release => release_time)
+        @info = lockit_dir.lock_info
+        DateTime.parse(@info[:release]).should be_instance_of(DateTime)
+      end
+    end
+    
+    it "reports its release time" do
+      @pending
+    end
+    
+    it "reports if no release time has been set" do
+      @pending
     end
     
   end
